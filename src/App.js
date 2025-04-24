e35e7a31f8bd89e26c16d3154e0aaeb63eb30e8d
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-04-24 10:20:17 +0530

    App.js Structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-   
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
