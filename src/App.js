4deafddd39790beb7bc96cc4c2f52e6678918ce5
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-05-04 10:40:54 +0530

    import registerpage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import Home from './pages/Home';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
