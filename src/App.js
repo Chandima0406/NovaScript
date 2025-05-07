55cd392384af19a2335e43aaf144fbe251375f0d
Prasanna Indunil <prasanna20012indunil@gmail.com>
2025-05-07 23:37:30 +0530

    Updated App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import Footer from './components/Footer';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/forgot-password" element={<div>Forgot Password Page (Placeholder)</div>} />
+          </Routes>
+          <Footer />
+        </div>
+      </div>
     </Router>
   );
 }
