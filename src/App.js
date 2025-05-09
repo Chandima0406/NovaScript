a20adbe69b2b0ac7cbff4bc1d7e082bdeedefc99
Chandima Wijerathna <153199095+Chandima0406@users.noreply.github.com>
2025-05-09 13:59:11 +0530

    update routers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+import SearchEngine from './pages/SearchEngine'; 
 import Footer from './components/Footer';
 import './App.css';
 
@@ -18,6 +19,7 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
+            <Route path="/search" element={<SearchEngine />} /> 
             <Route path="/forgot-password" element={<div>Forgot Password Page (Placeholder)</div>} />
           </Routes>
           <Footer />
