d94a8655048e203a0694face67bfaa129d734446
Vidumali Dahanayake <vidumalid@gmail.com>
2025-05-24 17:08:35 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,35 +1,46 @@
-// G:\NovaScript\client\src\api.js
 import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'http://localhost:5000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
-// Add token to requests if available
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+  // Handle FormData for multipart requests
+  if (config.data instanceof FormData) {
+    config.headers['Content-Type'] = 'multipart/form-data';
+  }
   return config;
 });
 
-// API functions
+// User
 export const register = (data) => API.post('/register', data);
 export const login = (data) => API.post('/login', data);
-export const publishProject = (data) => API.post('/projects', data);
+export const getUser = () => API.get('/user');
+
+// Projects
 export const getProjects = () => API.get('/projects');
+export const publishProject = (data) => API.post('/projects', data);
 export const getProjectById = (projectId) => API.get(`/projects/${projectId}`);
 export const updateProject = (projectId, data) => API.put(`/projects/${projectId}`, data);
-export const deleteProject = (projectId) => API.delete(`/projects/${projectId}`);
+export const getResearchPapers = () => API.get('/projects/user');
+export const deleteResearchPaper = (paperId) => API.delete(`/projects/${paperId}`);
+
+// Surveys
 export const createSurvey = (data) => API.post('/surveys', data);
-export const getSurveyById = (surveyId) => API.get(`/surveys/${surveyId}`);
 export const updateSurvey = (surveyId, data) => API.put(`/surveys/${surveyId}`, data);
-export const deleteSurvey = (surveyId) => API.delete(`/surveys/${surveyId}`);
-export const getUserSurveys = () => API.get('/surveys/user');
-export const getAnsweredSurveys = () => API.get('/surveys/user/answered');
+export const getSurveyById = (surveyId) => API.get(`/surveys/${surveyId}`);
 export const getAllSurveys = () => API.get('/surveys');
+export const getAnsweredSurveys = () => API.get('/surveys/user/answered');
 export const submitSurveyResponse = (surveyId, data) => API.post(`/surveys/${surveyId}/respond`, data);
+export const getUserSurveys = () => API.get('/surveys/user');
+export const deleteSurvey = (surveyId) => API.delete(`/surveys/${surveyId}`);
 export const getSurveyResponses = (surveyId) => API.get(`/surveys/${surveyId}/responses`);
 export const getSurveyAnalytics = (surveyId) => API.get(`/surveys/${surveyId}/analytics`);
 
